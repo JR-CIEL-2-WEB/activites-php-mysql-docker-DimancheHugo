6c55273ee2036810c851a76f686f38f034879a18
DimancheHugo <144660052+DimancheHugo@users.noreply.github.com>
2024-11-18 16:07:37 +0000

    modification du fichier statistique

--- code/series2/statistique.php
+++ code/series2/statistique.php
@@ -1,14 +1,13 @@
 <?php
-include 'Exercice_0.1.php';
+include_once 'Exercice_0.1.php';
 
-    function mediane($tab_note){
-        sort($tab_note);
-        $n = count($tab_note);
+    function mediane($salaire){
+        sort($salaire);
+        $n = count($salaire);
         if($n%2 == 0){
-            return ($tab_note[$n/2-1]+$tab_note[$n/2])/2;
+            return ($salaire[$n/2-1]+$salaire[$n/2])/2;
         }else{
-            return $tab_note[($n-1)/2];
+            return $salaire[($n-1)/2];
         }
     }
-    echo "La mediane est de ".mediane($tab_note).'/20<br>';
 ?>
