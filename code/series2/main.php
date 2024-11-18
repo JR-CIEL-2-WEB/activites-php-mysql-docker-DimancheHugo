6b9fada6cb3477d08f8ceeb6fbf15009f5b708b7
DimancheHugo <144660052+DimancheHugo@users.noreply.github.com>
2024-11-18 16:08:13 +0000

    ajout des fonction et écho pour afficher les bonnes réponse

--- code/series2/main.php
+++ code/series2/main.php
@@ -1,5 +1,17 @@
 <?php
-include 'Exercice_0.1.php';
-include 'statistique.php';
+include_once 'Exercice_0.1.php';
+include_once 'statistique.php';
 
+$salaire=array(1500,4500,2200,1500,3300,1800,1700,2000,4000);
+mediane($salaire);
+
+echo "La moyenne est de ".moyenne($tab_note).'/20<br>';
+echo "La mediane est de ".mediane($salaire)." euros<br>";
+
+$salaireNicolas = 2200;
+if($salaireNicolas<mediane($salaire)){
+    echo "Nicolas est en dessous de la mediane,il est donc dans les moins bien payés";
+}else{
+    echo "Nicolas est au dessus de la mediane,il est donc dans les mieux payés";
+}
 ?>
